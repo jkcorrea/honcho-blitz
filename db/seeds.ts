2e7fd9038e387f99a7deb6a3a8f6ce84e3d4704b
Jake Correa <jcorrea257@gmail.com>
2022-08-26 15:44:33 -0700

    basic table w/ mock data

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,15 +1,27 @@
-// import db from "./index"
+import Chance from 'chance';
 
-/*
- * This seed function is executed when you run `blitz db seed`.
- *
- * Probably you want to use a library like https://chancejs.com
- * to easily generate realistic data.
- */
+import { Prisma } from '.prisma/client';
+
+import db from './index';
+
+const chance = new Chance();
+
+const NUM_USERS = 20;
+
+// This seed function is executed when you run `blitz db seed`.
 const seed = async () => {
-  // for (let i = 0; i < 5; i++) {
-  //   await db.project.create({ data: { name: "Project " + i } })
-  // }
+  const allData = chance.n<Prisma.UserUncheckedCreateInput>(
+    () => ({
+      email: chance.email(),
+      name: chance.name({ full: true }),
+    }),
+    NUM_USERS,
+  );
+
+  // NOTE: sqlite doesn't support createMany, so we just need to map it into a transaction
+  const txs = allData.map((data) => db.user.create({ data }));
+  await db.$transaction(txs);
+  console.log('Done!');
 };
 
 export default seed;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,33 @@
 import { BlitzPage } from '@blitzjs/next';
 
+import userData from 'app/assets/mock_user_data.json';
 import { Table } from 'app/core/components/Table';
 import Layout from 'app/core/layouts/Layout';
+
+type TData = typeof userData[number];
 
 const Home: BlitzPage = () => {
   return (
     <Layout title="Home">
       <div className="container">
         <main>
-          <Table columns={[]} data={[]} />
+          <Table<TData>
+            columns={[
+              {
+                header: 'ID',
+                accessorKey: 'id',
+              },
+              {
+                header: 'Name',
+                accessorKey: 'name',
+              },
+              {
+                header: 'Email',
+                accessorKey: 'email',
+              },
+            ]}
+            data={userData}
+          />
         </main>
       </div>
     </Layout>
